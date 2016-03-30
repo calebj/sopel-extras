b0390ead86f66aae85562b40b454e8ef71e6a22c
Caleb Johnson <me@calebj.io>
2016-03-30 00:57:25 -0500

    Clean up 8ball module

--- 8ball.py
+++ 8ball.py
@@ -5,13 +5,33 @@ Licensed under the Eiffel Forum License 2.
 
 http://sopel.dfbta.net
 """
-import sopel
+from sopel.module import commands
 import random
-@sopel.module.commands('8')
+
+messages = ["It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes, definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy; try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "God says no.",
+            "Very doubtful.",
+            "Outlook not so good."
+            ]
+
+@commands('8')
 def ball(bot, trigger):
     """Ask the magic 8ball a question! Usage: .8 <question>"""
-    messages = ["It is certain"," It is decidedly so","Without a doubt","Yes definitely","You may rely on it","As I see it yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","God says no","Very doubtful","Outlook not so good"]
-    answer = random.randint(0,len(messages) - 1)
-    bot.say(messages[answer]);
-
-
+    answer = random.choice(messages)
+    bot.reply(answer);
